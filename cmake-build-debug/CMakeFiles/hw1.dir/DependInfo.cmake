5917625b5c10004b670743d88c3b8079152167e4
Lisa Geers <lizzgeers@hotmail.com>
2021-02-23 10:20:51 +0100

    added cmake

---
+++ cmake-build-debug/CMakeFiles/hw1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Vince/Documents/Geo1004/hw1/voxeliser/main.cpp" "/Users/Vince/Documents/Geo1004/hw1/cmake-build-debug/CMakeFiles/hw1.dir/voxeliser/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../voxeliser"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
