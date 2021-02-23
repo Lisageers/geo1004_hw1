443004ea841d53c5d3c483a14e0aaa9d5d9f94e2
Lisa Geers <lizzgeers@hotmail.com>
2021-02-23 12:41:04 +0100

    parsed faces and vertices

--- voxeliser/main.cpp
+++ voxeliser/main.cpp
@@ -16,14 +16,50 @@ bool intersects(const Point &orig, const Point &dest, const Point &v0, const Poi
 }
 
 int main(int argc, const char * argv[]) {
-  const char *file_in = "bag_bk.obj";
+  const char *file_in = "../bag_bk.obj";
   const char *file_out = "vox.obj";
   float voxel_size = 1.0;
   
   // Read file
   std::vector<Point> vertices;
   std::vector<std::vector<unsigned int>> faces;
-  // to do
+  std::string line;
+  float x, y, z;
+
+  std::ifstream input(file_in);
+  while(std::getline(input, line))
+  {
+//      std::cout << line[0] << "\n";
+      if (line[0] == 'v')
+      {
+          std::istringstream ss(line);
+          std::string word;
+          ss >> word;
+          ss >> word;
+          x = std::stof(word);
+          ss >> word;
+          y = std::stof(word);
+          ss >> word;
+          z = std::stof(word);
+          vertices.push_back(Point(x, y, z));
+
+      }
+      else if (line[0] == 'f')
+      {
+          std::istringstream ss(line);
+          std::string word;
+          std::vector<unsigned int> current_face;
+          ss >> word;
+          ss >> word;
+          current_face.push_back(std::stoi(word));
+          ss >> word;
+          current_face.push_back(std::stoi(word));
+          ss >> word;
+          current_face.push_back(std::stoi(word));
+          faces.push_back(current_face);
+      }
+  }
+  input.close();
   
   // Create grid
   Rows rows;
