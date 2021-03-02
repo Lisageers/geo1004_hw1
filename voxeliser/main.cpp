24e1825d8e595cd45d045785b13e516013919570
Lisa Geers <lizzgeers@hotmail.com>
2021-03-02 21:31:10 +0100

    voxelise done

--- voxeliser/main.cpp
+++ voxeliser/main.cpp
@@ -8,6 +8,7 @@
 #include "VoxelGrid.h"
 
 #include <math.h>
+#include <algorithm>
 
 float signed_volume(const Point &a, const Point &b, const Point &c, const Point &d) {
     Point crossproduct = (b-d).cross((c-d));
@@ -50,6 +51,7 @@ int main(int argc, const char * argv[]) {
   const char *file_in = "../bag_bk.obj";
   const char *file_out = "vox.obj";
   float voxel_size = 1.0;
+  float half_size = voxel_size/2;
   
   // Read file
   // initialise
@@ -109,18 +111,33 @@ int main(int argc, const char * argv[]) {
 
   // Create grid
   Rows rows;
-  float maxx, maxy, maxz;
+  float minx, miny, minz, maxx, maxy, maxz;
   for (int i; i < vertices.size(); i++)
   {
+      // update min x if found
+      if (vertices[i][0] < minx)
+      {
+          minx = vertices[i][0];
+      }
       // update max x if found
       if (vertices[i][0] > maxx)
       {
           maxx = vertices[i][0];
       }
+      // update min y if found
+      if (vertices[i][1] < miny)
+      {
+          miny = vertices[i][1];
+      }
       // update max y if found
       if (vertices[i][1] > maxy)
       {
           maxy = vertices[i][1];
+      }
+      // update min z if found
+      if (vertices[i][2] < minz)
+      {
+          minz = vertices[i][2];
       }
       // update max z if found
       if (vertices[i][2] > maxz)
@@ -129,58 +146,83 @@ int main(int argc, const char * argv[]) {
       }
   }
   // calculate number of rows in each dimension
-  rows.x = ceil(maxx / voxel_size);
-  rows.y = ceil(maxy / voxel_size);
-  rows.z = ceil(maxz / voxel_size);
+  rows.x = ceil((maxx-minx) / voxel_size);
+  rows.y = ceil((maxy-miny) / voxel_size);
+  rows.z = ceil((maxz-minz) / voxel_size);
 
   // use number of rows to make the grid
   VoxelGrid voxels(rows.x, rows.y, rows.z);
 
 
   // Voxelise
-  // initialise lines
+  // initialise
   std::vector<Point> line1, line2, line3;
+  std::vector<float> xlist, ylist, zlist;
 
-//   loop through each voxel
-    for (int ziter = 0; ziter < rows.z; ziter++)
+   // loop through triangles
+  for (auto const &triangle: faces)
     {
-        for (int yiter = 0; yiter < rows.y; yiter++)
-        {
-            for (int xiter = 0; xiter < rows.x; xiter++)
-            {
-                // create intersection targets
-                line1 = {Point(xiter, yiter+0.5, ziter+0.5), Point(xiter + 1, yiter + 0.5, ziter+0.5)};
-                line2 = {Point(xiter + 0.5, yiter, ziter+0.5), Point(xiter + 0.5, yiter + 1, ziter+0.5)};
-                line3 = {Point(xiter + 0.5, yiter + 0.5, ziter), Point(xiter + 0.5, yiter + 0.5, ziter+1)};
+        int startx, endx, starty, endy, startz, endz;
+        // creat lists with x, y and z values of the vertices
+        xlist = {vertices[triangle[0]-1][0], vertices[triangle[1]-1][0], vertices[triangle[2]-1][0]};
+        ylist = {vertices[triangle[0]-1][1], vertices[triangle[1]-1][1], vertices[triangle[2]-1][1]};
+        zlist = {vertices[triangle[0]-1][2], vertices[triangle[1]-1][2], vertices[triangle[2]-1][2]};
 
-                for (auto const &triangle: faces)
+        // calculate the start and end voxel numbers in all dimensions for the bounding box
+        startx = floor((std::min({xlist[0], xlist[1], xlist[2]}) - minx) / voxel_size);
+        endx = ceil((std::max({xlist[0], xlist[1], xlist[2]}) - minx) / voxel_size);
+        starty = floor((std::min({ylist[0], ylist[1], ylist[2]}) - miny) / voxel_size);
+        endy = ceil((std::max({ylist[0], ylist[1], ylist[2]}) - miny) / voxel_size);
+        startz = floor((std::min({zlist[0], zlist[1], zlist[2]}) - minz) / voxel_size);
+        endz = ceil((std::max({zlist[0], zlist[1], zlist[2]}) - minz) / voxel_size);
+
+        // calculate ranges
+        int xdiff = endx-startx;
+        int ydiff = endy-starty;
+        int zdiff = endz-startz;
+
+        // loop though bounding box
+        for (int ziter = 0; ziter <= zdiff; ziter++)
+        {
+            for (int yiter = 0; yiter <= ydiff; yiter++)
+            {
+                for (int xiter = 0; xiter <= xdiff; xiter++)
                 {
+                    // calculate coordinates of the voxel
+                    float xvoxel = (xiter + startx) * voxel_size + minx;
+                    float yvoxel = (yiter + starty) * voxel_size + miny;
+                    float zvoxel = (ziter + startz) * voxel_size + minz;
+
+                    // create intersection targets, start and end points
+                    line1 = {Point(xvoxel, yvoxel+half_size, zvoxel+half_size), Point(xvoxel + voxel_size, yvoxel + half_size, zvoxel+half_size)};
+                    line2 = {Point(xvoxel + half_size, yvoxel, zvoxel+half_size), Point(xvoxel + half_size, yvoxel + voxel_size, zvoxel+half_size)};
+                    line3 = {Point(xvoxel + half_size, yvoxel + half_size, zvoxel), Point(xvoxel + half_size, yvoxel + half_size, zvoxel+voxel_size)};
+
+
+                    // check if triangle intersects any of the intersection targets, make voxel 1 if intersect
                     if (intersects(line1[0], line1[1], vertices[triangle[0]-1], vertices[triangle[1]-1], vertices[triangle[2]-1]))
                     {
-                        voxels(xiter, yiter, ziter) = 1;
+                        voxels((xiter + startx), yiter, (ziter + startz)) = 1;
                     }
                     else if (intersects(line2[0], line2[1], vertices[triangle[0]-1], vertices[triangle[1]-1], vertices[triangle[2]-1]))
                     {
-                        voxels(xiter, yiter, ziter) = 1;
+                        voxels((xiter + startx), (yiter + starty), (ziter + startz)) = 1;
                     }
                     else if (intersects(line3[0], line3[1], vertices[triangle[0]-1], vertices[triangle[1]-1], vertices[triangle[2]-1]))
                     {
-                        voxels(xiter, yiter, ziter) = 1;
+                        voxels((xiter + startx), (yiter + starty), (ziter + startz)) = 1;
                     }
-
                 }
-
-
             }
         }
     }
 
 // for testing
-//    std::vector<Point> line1{Point(0, 0.5, 0.5), Point(1, 0.5, 0.5)};
-//    std::vector<Point> line2{Point(0.5, 0, 0.5), Point(0.5, 1, 0.5)};
-//    std::vector<Point> line3{Point(0.5, 0.5, 0), Point(0.5, 0.5, 1)};
-//    std::vector<Point> line4{Point(50, 50, 50), Point(51, 50, 50)};
-//    bool inter = intersects(line4[0], line4[1], Point(0.5, 0.5, 0), Point(0.5, 0.5, 1), Point(0.5, 1, 0.5));
+//    std::vector<Point> line11{Point(0, 0.5, 0.5), Point(1, 0.5, 0.5)};
+//    std::vector<Point> line22{Point(0.5, 0, 0.5), Point(0.5, 1, 0.5)};
+//    std::vector<Point> line33{Point(0.5, 0.5, 0), Point(0.5, 0.5, 1)};
+//    std::vector<Point> line44{Point(50, 50, 50), Point(51, 50, 50)};
+//    bool inter = intersects(line11[0], line11[1], Point(0.5, 0.5, 0), Point(0.5, 0.5, 1), Point(0.5, 1, 0.5));
 //    std::cout << inter;
 
 
@@ -188,7 +230,7 @@ int main(int argc, const char * argv[]) {
   // to do
   
   // Write voxels
-  // to do
+
   
   return 0;
 }
